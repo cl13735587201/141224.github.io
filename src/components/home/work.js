0e3b67a72ff55f86ec2677caa3ea567fef151048
chenli <chenli@ifugle.com>
2021-03-01 09:59:48 +0800

    修改：结构调整

---
+++ src/components/home/work.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class start extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+    componentDidMount() {
+    }
+    render() {
+        return (
+            <div className="C work">
+             </div>
+        )
+    }
+}
+export default start
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 // import { Link } from "react-router-dom"
 import Start from "../components/home/start"
-import Run from "../components/home/run"
+import Work from "../components/home/work"
 import Kitty from "../components/home/kitty";
+import Run from "../components/home/run"
 import Hobby from "../components/home/hobby";
 import {Steps} from 'antd';
-import { SmileOutlined, CrownOutlined, BugOutlined } from '@ant-design/icons';
+import { SmileOutlined, CrownOutlined, BugOutlined, CoffeeOutlined } from '@ant-design/icons';
 
 const {Step} = Steps;
 
@@ -48,21 +49,27 @@ class home extends React.Component {
             this.setState({
                 current: 3,
             });
+        } else if (scrollTop > 2400 && scrollTop < 3000) {
+            this.setState({
+                current: 4,
+            });
         }
-}
+    }
     render() {
         const {current} = this.state;
         return (
             <div className ="P home">
                 <Steps current={current} onChange={this.onChange} direction="vertical">
                     <Step title="关键词" icon={<SmileOutlined twoToneColor="#eb2f96"/>}/>
-                    <Step title="健身" icon={<CrownOutlined twoToneColor="#66CD00"/>} description=""/>
+                    <Step title="工作" icon={<CrownOutlined twoToneColor="#66CD00"/>} description=""/>
                     <Step title="宠物" icon={<BugOutlined twoToneColor="#FFA042"/>} description=""/>
-                    <Step title="兴趣" icon={<BugOutlined twoToneColor="#FFA042"/>} description=""/>
+                    <Step title="健身" icon={<CrownOutlined twoToneColor="#66CD00"/>} description=""/>
+                    <Step title="兴趣" icon={<CoffeeOutlined twoToneColor="#FFA042"/>} description=""/>
                 </Steps>
                 <Start></Start>
-                <Run></Run>
+                <Work></Work>
                 <Kitty></Kitty>
+                <Run></Run>
                 <Hobby></Hobby>
             </div>
         )
