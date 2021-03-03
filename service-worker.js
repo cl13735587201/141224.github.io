864a8be518fda375f39cf1611431aa985d9aeac8
chenli <chenli@ifugle.com>
2021-03-03 19:45:13 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20d74bf3703cb648b57b3fdf3a0a3ef8.js"
+  "/precache-manifest.6242d826c3dcfd29cfc388d5d8591983.js"
 );
 
 self.addEventListener('message', (event) => {
